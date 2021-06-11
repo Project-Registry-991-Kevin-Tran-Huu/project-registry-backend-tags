50f7ef93465318ebce04214d8d1ec8d9031ac052
Kemo Allen <kemo@claudia-allens-iMac-5.local>
2021-06-11 17:04:39 -0500

    First commit

---
+++ src/main/java/com/revature/registry/ProjectRegistryTagsApplication.java
@@ -0,0 +1,13 @@
+package com.revature.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRegistryTagsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRegistryTagsApplication.class, args);
+	}
+
+}
